9b87eea3d1c51a0c263fe6bffe7917bcfcc0434e
Gaurav Sablok <gauravcodepro@gmail.com>
2024-10-20 23:30:28 +0200

    codecreatede
    started writing the code for a entire metagenome analyzer

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # go-metagenomics-deep
+
+- a complete metagenome analyzer from long reads to genome assembly, metagenome mags, genome annotation and placement. 
+- asynchronous program
+
+Gaurav Sablok
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+
+Author Gaurav Sablok
+Universitat Potsdam
+Date 2024-10-21
+
+A complete metagenomics analysis in golang which calls long reads, genome assembly, mags and annotations of the genome.
+
+
+*/
